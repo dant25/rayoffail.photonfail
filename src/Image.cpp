ede606a52b352026f65f113c265d450c57f1713e
caio <caio@localhost>
2010-05-18 21:32:24 +0000

    Adicionado uma lib pra ler .tga.    - Fazer a pr?pria fun??o de ler tga depois, se der tempo e for necess?rio

--- src/Image.cpp
+++ src/Image.cpp
@@ -1,4 +1,6 @@
 #include "Image.h"
+//FIXME escrever a função de ler tga ou usar outra lib
+#include "image.h"
 #include <stdio.h>
 
 Image::Image(int width, int height) : width(width), height(height) {
@@ -8,9 +10,13 @@ Image::Image(int width, int height) : width(width), height(height) {
 }
 
 Image::~Image() {
-    for(int i = 0; i < width; i++)
-        delete []color[i];
-    delete []color;
+   this->clear();
+}
+
+void Image::clear() {
+   for(int i = 0; i < width; i++)
+      delete []color[i];
+   delete []color;
 }
 
 int Image::getWidth() const {
@@ -25,7 +31,7 @@ void Image::setPixel(int x, int y, const SpectralQuantity& c) {
     color[x][y] = c;
 }
 
-SpectralQuantity Image::getPixel(int x, int y){
+SpectralQuantity Image::getPixel(int x, int y) const {
 	return color[x][y];
 }
 
@@ -81,4 +87,29 @@ void Image::save(const char* path) {
 		}
 	}
 	fclose(file);
+}
+
+#include <iostream>
+void Image::load(const char* path) {
+   //FIXME verificar se é necessário alocar memória
+   clear(); 
+
+   unsigned char* data = NULL;
+   int x, y, n;
+   data = stbi_load(path, &x, &y, &n, 0);
+   width = x;
+   height = y;
+   std::cout << "carregou a img" << std::endl;
+
+   color = new SpectralQuantity*[width];
+   for(int i = 0; i < width; i++)
+      color[i] = new SpectralQuantity[height];
+   
+   for(int j = 0; j < height; j++)
+      for(int i = 0; i < width; i++)
+         color[i][j] = SpectralQuantity(data[3*j*width + i*3]/255.0,
+                                        data[3*j*width + i*3 + 1]/255.0,
+                                        data[3*j*width + i*3 + 2]/255.0);
+
+   stbi_image_free(data);
 }
